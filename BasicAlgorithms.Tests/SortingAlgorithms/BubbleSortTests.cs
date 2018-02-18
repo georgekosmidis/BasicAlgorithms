d1ee94fe225bff277b3ffa5ed2fdcb46ee7167f6
George Kosmidis <georgekosmidis@outlook.com>
2018-02-18 11:00:42 +0200

    bubble sort added

---
+++ BasicAlgorithms.Tests/SortingAlgorithms/BubbleSortTests.cs
@@ -0,0 +1,26 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using BasicAlgorithms.SortingAlgorithms;
+
+namespace BasicAlgorithms.Tests
+{
+    [TestClass]
+    public class BubbleSortTests
+    {
+        [TestMethod]
+        public void BubbleSort_Sort()
+        {
+            var search = new BubbleSort();
+
+            var list = new List<int>() { 5, 1, 3, 2, 4 };
+            var result = search.Sort(list);
+            Assert.AreEqual(1, result.SortedData[0]);
+            Assert.AreEqual(2, result.SortedData[1]);
+            Assert.AreEqual(3, result.SortedData[2]);
+            Assert.AreEqual(4, result.SortedData[3]);
+            Assert.AreEqual(5, result.SortedData[4]);
+        }
+
+       
+    }
+}
---
+++ BasicAlgorithms/SortingAlgorithms/BubbleSort.cs
@@ -0,0 +1,40 @@
+﻿using BasicAlgorithms.SortingAlgorithms.Interfaces;
+using BasicAlgorithms.SortingAlgorithms.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BasicAlgorithms.SortingAlgorithms
+{
+    public class BubbleSort : ISort
+    {
+
+        /// <summary>
+        /// Bubble Sort Algorithm [Time: O(n^2), Space: O(1)]
+        /// </summary>
+        /// <param name="data"></param>
+        /// <returns></returns>
+        public SortResults Sort(List<int> data)
+        {
+            var results = new SortResults();
+            var watch = System.Diagnostics.Stopwatch.StartNew();
+            for (var i = 0; i < data.Count; i++)
+            {
+                for (var j = 0; j < data.Count - 1; j++)
+                {
+                    results.Cycles++;
+                    if (data[j] > data[j + 1])
+                    {
+                        var tmp = data[j];
+                        data[j] = data[j + 1];
+                        data[j + 1] = tmp;
+                    }
+                }
+            }
+            results.SortedData = data;
+            watch.Stop();
+            results.Ticks = watch.ElapsedMilliseconds;
+            return results;
+        }
+    }
+}
